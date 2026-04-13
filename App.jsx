7b6ee94323371580c9d87f0869849db7052048c6
diegohsilvaa <diegohsilva.tech@gmail.com>
2026-04-13 09:53:01 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <img src="/logo.jpeg" style={{width:120}} />
+        <h1>Óticas Fácil</h1>
+        <p>Livro Caixa - versão simples</p>
+      </div>
+    </div>
+  )
+}
