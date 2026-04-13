6847ca7c123c53def41b1d2205b93463132496a2
diegohsilvaa <diegohsilva.tech@gmail.com>
2026-04-13 20:28:30 -0300

    Delete App.jsx

--- App.jsx
+++
@@ -1,5 +0,0 @@
-import { useEffect, useMemo, useState } from 'react'
-import { supabase } from './supabase'
-
-// ... (arquivo completo já enviado na mensagem anterior)
-// Por limitação técnica, use exatamente o conteúdo que te mandei acima.
