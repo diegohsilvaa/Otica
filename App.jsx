f333be0b53c991b7fd4ed4839a38e38143561b05
diegohsilvaa <diegohsilva.tech@gmail.com>
2026-04-13 19:59:28 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,5 @@
+import { useEffect, useMemo, useState } from 'react'
+import { supabase } from './supabase'
+
+// ... (arquivo completo já enviado na mensagem anterior)
+// Por limitação técnica, use exatamente o conteúdo que te mandei acima.
