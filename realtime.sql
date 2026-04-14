3a3bbf125b9b30e4dfb0e04f5d2e71aab84724b4
diegohsilvaa <diegohsilva.tech@gmail.com>
2026-04-14 14:41:17 -0300

    Delete realtime.sql

--- realtime.sql
+++
@@ -1,4 +0,0 @@
-alter publication supabase_realtime add table public.lancamentos_caixa;
-alter publication supabase_realtime add table public.aberturas_caixa;
-alter publication supabase_realtime add table public.fechamentos_caixa;
-alter publication supabase_realtime add table public.clientes;
