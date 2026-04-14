3da7d26e79a5901fdc40124d0b33296c3aab832d
diegohsilvaa <diegohsilva.tech@gmail.com>
2026-04-14 14:39:53 -0300

    Add files via upload

---
+++ realtime.sql
@@ -0,0 +1,4 @@
+alter publication supabase_realtime add table public.lancamentos_caixa;
+alter publication supabase_realtime add table public.aberturas_caixa;
+alter publication supabase_realtime add table public.fechamentos_caixa;
+alter publication supabase_realtime add table public.clientes;
